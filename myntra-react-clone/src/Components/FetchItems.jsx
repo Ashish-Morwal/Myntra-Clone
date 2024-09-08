b0387771fd7f598a1a10401b8f498065fecca545
Ashish Morwal <ashishmorwal001@gmail.com>
2024-09-08 22:40:03 +0530

    adding new changes

--- myntra-react-clone/src/Components/FetchItems.jsx
+++ myntra-react-clone/src/Components/FetchItems.jsx
@@ -3,30 +3,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { itemsActions } from "../Store/ItemsSlice";
 import { fetchStatusActions } from "../Store/fetchStatusSlice";
 
-const prodURL = "https://myntra-clone-vpdq.onrender.com";
-const baseURL = "http://localhost:8080/items";
+const baseURL = "https://myntra-clone-vpdq.onrender.com/items";
 
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
   const dispatch = useDispatch();
+
   useEffect(() => {
     if (fetchStatus.fetchDone) return;
     const controller = new AbortController();
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchStarted());
-    fetch(`${prodURL}`, { signal })
+    fetch(baseURL, { signal }) // Corrected this line
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
         dispatch(fetchStatusActions.markFetchFinished());
         dispatch(itemsActions.addInitialItems(items[0]));
+      })
+      .catch((error) => {
+        // Handle errors, such as fetch being aborted
+        console.error("Fetch error: ", error);
+        dispatch(fetchStatusActions.markFetchFinished()); // To ensure the state is updated
       });
 
     return () => {
-      controller.abort();
+      controller.abort(); // This will abort the fetch when the component unmounts
     };
-  }, [fetchStatus]);
+  }, [fetchStatus, dispatch]); // Added `dispatch` to dependencies
+
   return <></>;
 };
+
 export default FetchItems;
